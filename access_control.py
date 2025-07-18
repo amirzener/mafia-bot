c35e8eeeb35982ced81a3e8d1e3ca5ce97b36dc7
amirzener <amirzener2@gmail.com>
2025-07-18 06:21:03 +0330

    Update access_control.py

--- access_control.py
+++ access_control.py
@@ -1,25 +1,25 @@
 from data_manager import DataManager
-from config import ADMINS_FILE, ROLE_OWNER, ROLE_SUPER_ADMIN, ROLE_ADMIN
+from config import ADMINS_FILE, ROLE_OWNER, ROLE_SUPER_ADMIN, ROLE_ADMIN, OWNER_ID
 
 class AccessControl:
     @staticmethod
     def is_owner(user_id):
-        """بررسی مالک بودن کاربر"""
-        return str(user_id) == str(os.getenv('OWNER_ID'))
+        """بررسی اینکه کاربر مالک ربات است"""
+        return str(user_id) == str(OWNER_ID)
 
     @staticmethod  
     def is_super_admin(user_id):  
-        """بررسی سوپر ادمین بودن کاربر"""
+        """بررسی سوپر ادمین بودن"""
         admins = DataManager.load_data(ADMINS_FILE)  
         return str(user_id) in admins and admins[str(user_id)]["role"] == ROLE_SUPER_ADMIN  
 
     @staticmethod  
     def is_admin(user_id):  
-        """بررسی ادمین بودن کاربر"""
+        """بررسی ادمین بودن"""
         admins = DataManager.load_data(ADMINS_FILE)  
         return str(user_id) in admins and admins[str(user_id)]["role"] in [ROLE_ADMIN, ROLE_SUPER_ADMIN]  
 
     @staticmethod  
     def is_privileged(user_id):  
-        """بررسی دسترسی ویژه کاربر"""
+        """بررسی دسترسی ویژه"""
         return AccessControl.is_owner(user_id) or AccessControl.is_admin(user_id)
