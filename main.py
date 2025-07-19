0afdf193b95ee85c477883da21bb9e49dc2e43c2
amirzener <amirzener2@gmail.com>
2025-07-19 08:11:12 +0330

    Update main.py

--- main.py
+++ main.py
@@ -141,4 +141,5 @@ async def set_webhook():
 
 if __name__ == '__main__':
     asyncio.run(set_webhook())
-    app.run(host='0.0.0.0', port=8080)
+    port = int(os.environ.get("PORT", 8080))
+    app.run(host="0.0.0.0", port=port)
