e5750c19d5dea2f8657c42332ff1389ec9d526cc
amirzener <amirzener2@gmail.com>
2025-07-22 10:58:43 +0330

    Update main.py

--- main.py
+++ main.py
@@ -490,13 +490,8 @@ async def hastam_command(update: Update, context: ContextTypes.DEFAULT_TYPE):
     await update_active_list_message(list_id, context)
 
     await update.message.reply_text("✅ شما به عنوان بازیکن ثبت نام شدید.")
-async def check_hastam(update, context):
-    if update.message.text.strip() =="ثبت نام":
-        await hastam_callback(update, context)
 
 
-application.add_handler(CommandHandler("hastam", hastam_command))
-# تنظیم هندلرها
 application.add_handler(CommandHandler("start", start_command))
 application.add_handler(CommandHandler("menu", menu_command))
 application.add_handler(CommandHandler("list", list_command))
